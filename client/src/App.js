62283e17c7dc32b475cd0f2d022ad85b643ec50d
Swathi Raket <swathiraket@gmail.com>
2026-02-18 13:25:37 +0530

    Finalize Bellcorp frontend + dashboard fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
